e155eb473794825cf25d4516cfc8e0ec8691da06
Nicole <nicolejunelaurin@gmail.com>
2023-07-21 11:07:24 -0400

    commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Profound/precache-manifest.932de8a4317c29508de3cdb9e4d482bb.js"
+  "/Profound/precache-manifest.abf8d5f367c7819f2d595316bc3bf3d0.js"
 );
 
 workbox.clientsClaim();
